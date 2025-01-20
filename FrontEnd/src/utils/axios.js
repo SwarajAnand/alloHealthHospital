eddde86829d72ebd63687cd8dc18b6f794f1376d
Swaraj Anand <swrjnnd@gmail.com>
2025-01-20 14:17:33 +0530

    updated base url

--- FrontEnd/src/utils/axios.js
+++ FrontEnd/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://allohealthhospital.onrender.com/api',
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://allohealthhospital.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api'
 });
 
 instance.interceptors.request.use(
